081ea0006ae43cf99252436cf35648b14c8bd6a2
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2022-11-02 13:27:07 -0400

    tiendaOnlineConMaven

--- src/main/java/com/pildorasInformaticas/App.java
+++ src/main/java/com/pildorasInformaticas/App.java
@@ -24,13 +24,40 @@ public class App
                     .buildSessionFactory();
             Session miSession = configuration.openSession();
             try {
-                Cliente cliente1 = new Cliente("Juan","Almicar","Sprinfield#12");
-                DetallesCliente detallesCliente1 = new DetallesCliente("www.pildorasInformaticas","5645","primer cliente");
-                cliente1.setDetallesCliente(detallesCliente1);
+//                Cliente cliente1 = new Cliente("Juan","Almicar","Sprinfield#12");
+//                DetallesCliente detallesCliente1 = new DetallesCliente("www.pildorasInformaticas","5645","primer cliente");
+//                cliente1.setDetallesCliente(detallesCliente1);
+//                miSession.beginTransaction();
+//                miSession.save(cliente1);
+//                miSession.getTransaction().commit();
+//                System.out.println("Cliente registrado");
+
+//                miSession.beginTransaction();
+//
+//                //Obtener cliente
+//                Cliente cliente = miSession.get(Cliente.class,1);
+//
+//                //Crear Pedidos
+//                Pedido p1 = new Pedido(new GregorianCalendar(2022,7,5));
+//                Pedido p2 = new Pedido(new GregorianCalendar(2022,8,1));
+//                Pedido p3 = new Pedido(new GregorianCalendar(2022,9,7));
+//
+//                //Agrega pedidos
+//                cliente.agregarPedidos(p1);
+//                cliente.agregarPedidos(p2);
+//                cliente.agregarPedidos(p3);
+//
+//                //Guardar los pedidos
+//                miSession.save(p1);
+//                miSession.save(p2);
+//                miSession.save(p3);
+//
+//                miSession.getTransaction().commit();
+
                 miSession.beginTransaction();
-                miSession.save(cliente1);
+                Cliente cliente = miSession.get(Cliente.class,1);
+                System.out.println(cliente.getPedidos());
                 miSession.getTransaction().commit();
-                System.out.println("Cliente registrado");
             }catch (Exception e){
                 System.out.println("Cliente no registrado");
             }finally {
